61b752b5bc905140e5c1538631314294b919a0aa
EdwardisTilted <jedjian0520@gmail.com>
2026-01-27 18:03:43 +0800

    Update CurrencyConverterGUI.java

--- CurrencyConverterGUI.java
+++ CurrencyConverterGUI.java
@@ -2,8 +2,19 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-public class CurrencyConverterGUI
+public class CurrencyConverterGUI implements ActionListener
 {
+    private JFrame 
+    private JPanel
+    private JButton
+    private JLabel
 
-    
+    public void startScreen1()
+    {
+
+    }
+    public void startScreen2()
+    {
+        
+    }
 }
